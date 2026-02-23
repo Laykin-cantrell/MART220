88b1df883adc0edc7b1d50f11d2c3ce31e131ff9
Laykin-cantrell <laykinlcantrell@gmail.com>
2026-02-23 14:16:58 -0700

    Changed lemons

--- Homework 5/js/sketch.js
+++ Homework 5/js/sketch.js
@@ -1,19 +1,19 @@
-var numberOfLemons = 8;
+var numberOfLemons = 10;
 var lemonX = [];
 var lemonY = [];
 var lemonSize = [];
 var lemonImg;
 
 function preload() {
-    lemonImg = loadImg("images/lemon.png");
+    lemonImg = loadImage("images/lemon.png");
 }
 
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(700, 500);
 
     for (var i = 0; i < numberOfLemons; i++) {
-        lemonX[i] = random(60, width - 60);
-        lemonY[i] = random(100, height - 80);
+        lemonX[i] = random(60, width - 70);
+        lemonY[i] = random(100, height - 70);
         lemonSize[i] = random(60, 85);
      }
 }
@@ -24,7 +24,7 @@ function draw() {
     for (var i = 0; i < numberOfLemons; i++) {
         var animatedY = lemonY[i] + sin(frameCount * 0.05 + i) * 12;
 
-        imageMode(Center);
+        imageMode(CENTER);
         image(lemonImg, lemonX[i], animatedY, lemonSize[i], lemonSize[i]);
     }
 }
