74c9c8a86c93b7d465d9e301b6620ecae946fd26
Laykin-cantrell <laykinlcantrell@gmail.com>
2026-03-02 22:56:26 -0700

    Update sketch.js

--- Homework 7/js/sketch.js
+++ Homework 7/js/sketch.js
@@ -1,1 +1,138 @@
+let lemons = [];
+let numLemons = 6;
 
+// pigeon animation frames
+let pigeonFrames = [];
+let frameIndex = 0;
+
+// pigeon position
+let birdX = 300;
+let birdY = 200;
+let moveSpeed = 2;
+
+// food image
+let lemonImg;
+
+// score and timer
+let score = 0;
+let gameState = "playing";
+let totalTime = 60; 
+let startTime = 0;
+
+function preload() {
+    // pigeon frames
+    for (let i = 1; i <=22; i++) {
+        pigeonFrames.push(loadImage("image/pigeon_" + i + ".png"));
+    }
+
+    // lemon image
+    lemonImg = loadImage("images/lemon.png");
+}
+
+function setup() {
+    for (let i = 0; i < numLemons; i++) {
+        let x = random(60, width - 60);
+        let y = random(100, height - 60);
+        let s = random(60, 90);
+
+        lemons.push(new Lemon(x, y, s));
+    }
+
+    startTime = millis();
+}
+
+function draw() {
+    background(196, 241, 255);
+
+    // timer
+    let elapsedSeconds = floor((millis() - startTime) / 1000);
+    let timeLeft = max(0, totalTime - elapsedSeconds);
+
+    if (timeLeft === 0) {
+        gameState = "gameOver";
+    }
+
+    // display lemons
+    for (let i = 0; i < lemons.length; i++) {
+        lemons[i].update();
+        lemons[i].display();
+    }
+
+    // pigeon animation
+    let moving = false;
+
+    if (gameState === "playing") {
+        if (keyIsDown(65)) { // A
+        birdX -= moveSpeed;
+        moving = true;
+        }
+        if (keyIsDown(68)) { // D
+        birdX += moveSpeed;
+        moving = true;
+        }
+        if (keyIsDown(87)) { // W
+        birdY -= moveSpeed;
+        moving = true;
+        }
+        if (keyIsDown(83)) { // S
+        birdY += moveSpeed;
+        moving = true;
+        }
+    }
+
+    birdX = constrain(birdX, 0, width);
+    birdY = constrain(birdY, 0, height);
+
+    // speed
+    let currentSpeed;
+    if (moving) {
+        currentSpeed = 10;
+    } 
+    else {
+        currentSpeed = 14;
+    }
+
+    if (frameCount % currentSpeed === 0) {
+        frameIndex = (frameIndex + 1) % pigeonFrames.length;
+    }
+
+    imageMode(CENTER);
+    image(pigeonFrames[frameIndex], birdX, birdY, 160, 160);
+
+    // score
+    if (gameState === "playing") {
+        for (let i = 0; i < lemons.length; i++) {
+            let birdRadius = 55;
+            let lemonRadius = lemons[i].s * 0.5;
+
+            let d = dist(birdX, birdY, lemons[i].x, lemons[i].y);
+
+            if (d < birdRadius + lemonRadius) {
+                score++;
+                lemons[i].respawn();
+            }
+        }
+    }
+
+    // text
+    FileList(0);
+    textSize(16);
+    text("Score: " + score, 10, 25);
+    text("Time: " + timeLeft, width - 110, 25);
+
+    textSize(14);
+    text("Move the pigeon with WASD", 10, 50);
+
+    // game over screen
+    if (gameState === "gameOver") {
+        textAlign(CENTER, CENTER);
+        textSize(48);
+        fill(0);
+        text("GAME OVER", width / 2, height / 2);
+
+        textSize(24);
+        text("Final Score: " + score, width / 2, height / 2 + 60);
+
+        textAlign(LEFT, BASELINE);
+    }
+}
