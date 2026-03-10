ee5e460c75395ed7b06cfab0594a4b5d2465b064
Laykin-cantrell <laykinlcantrell@gmail.com>
2026-03-09 23:39:49 -0600

    added sound

---
+++ Homework 8/js/sketch.js
@@ -0,0 +1,235 @@
+let lemons = [];
+let numLemons = 6;
+
+// pigeon animation frames
+let pigeonFrames = [];
+let frameIndex = 0;
+
+// pigeon position
+let birdX = 300;
+let birdY = 200;
+let moveSpeed = 2;
+
+// food image
+let lemonImg;
+let orangeImg;
+
+// score and timer
+let score = 0;
+let gameState = "playing";
+let totalTime = 60; 
+let startTime = 0;
+
+// sounds
+let music;
+let goodSound;
+let badSound;
+
+// bad food
+let oranges = [];
+
+// health
+let health = 100;
+
+function preload() {
+    // pigeon frames
+    for (let i = 1; i <=22; i++) {
+        pigeonFrames.push(loadImage("images/pigeon/pigeon_" + i + ".png"));
+    }
+
+    // lemon image
+    lemonImg = loadImage("images/lemon/lemon.png");
+
+    // orange image
+    orangeImg = loadImage("images/orange/orange.png");
+
+    // sounds
+    music = loadSound("sounds/background_music.wav");
+    goodSound = loadSound("sounds/good_sound.mp3");
+    badSound = loadSound("sounds/bad_sound.wav");
+}
+
+function setup() {
+    createCanvas(700, 500);
+
+    music.setVolume(0.2);
+    goodSound.setVolume(0.8);
+    badSound.setVolume(3.5);
+
+    // spawn lemons
+    for (let i = 0; i < numLemons; i++) {
+        let x = random(60, width - 60);
+        let y = random(100, height - 60);
+        let s = random(60, 90);
+
+        lemons.push(new Lemon(x, y, s));
+    }
+
+    // spawn oranges
+    for (let i = 0; i < 3; i++) {
+        oranges.push({
+            x: random(60, width - 60),
+            y: random(100, height - 60)
+        });
+    }
+
+    startTime = millis();
+}
+
+function draw() {
+    background(196, 241, 255);
+
+    // timer
+    let elapsedSeconds = floor((millis() - startTime) / 1000);
+    let timeLeft = max(0, totalTime - elapsedSeconds);
+
+    if (timeLeft === 0 || health <= 0) {
+        gameState = "gameOver";
+    }
+
+    // display lemons
+    for (let i = 0; i < lemons.length; i++) {
+        lemons[i].display();
+    }
+
+    // display oranges
+    for (let i = 0; i < oranges.length; i++) {
+        image(orangeImg, oranges[i].x, oranges[i].y, 70, 70);
+    }
+
+    // pigeon animation
+    let moving = false;
+
+    if (gameState === "playing") {
+        if (keyIsDown(65)) { // A
+        birdX -= moveSpeed;
+        moving = true;
+        }
+        if (keyIsDown(68)) { // D
+        birdX += moveSpeed;
+        moving = true;
+        }
+        if (keyIsDown(87)) { // W
+        birdY -= moveSpeed;
+        moving = true;
+        }
+        if (keyIsDown(83)) { // S
+        birdY += moveSpeed;
+        moving = true;
+        }
+    }
+
+    birdX = constrain(birdX, 0, width);
+    birdY = constrain(birdY, 0, height);
+
+    // speed
+    let currentSpeed;
+    if (moving) {
+        currentSpeed = 10;
+    } 
+    else {
+        currentSpeed = 14;
+    }
+
+    if (frameCount % currentSpeed === 0) {
+        frameIndex = (frameIndex + 1) % pigeonFrames.length;
+    }
+
+    imageMode(CENTER);
+    image(pigeonFrames[frameIndex], birdX, birdY, 160, 160);
+
+    // score
+    if (gameState === "playing") {
+        for (let i = 0; i < lemons.length; i++) {
+            let d = dist(birdX, birdY, lemons[i].x, lemons[i].y);
+
+            if (d < 60) {
+                score++;
+
+                if (!goodSound.isPlaying()) {
+                    goodSound.play();
+                }
+
+                lemons[i].x = random(60, width - 60);
+                lemons[i].y = random(100, height - 60);
+            }
+        }
+
+        // orange collision
+        for (let i = 0; i < oranges.length; i++) {
+            let d = dist(birdX, birdY, oranges[i].x, oranges[i].y);
+
+            if (d < 60) {
+                health -= 10;
+
+                if (!badSound.isPlaying()) {
+                    badSound.play();
+                }
+
+                oranges[i].x = random(60, width - 60);
+                oranges[i].y = random(100, height - 60);
+            }
+        }
+    }
+
+    // text
+    fill(0);
+    textSize(16);
+    text("Score: " + score, 10, 25);
+    text("Health: " + health, 10, 50);
+    text("Time: " + timeLeft, width - 110, 25);
+
+    textSize(14);
+    text("Move the pigeon with WASD", 10, 75);
+
+    // game over screen
+    if (gameState === "gameOver") {
+        textAlign(CENTER, CENTER);
+        textSize(48);
+        fill(0);
+        text("GAME OVER", width / 2, height / 2);
+
+        textSize(24);
+        text("Final Score: " + score, width / 2, height / 2 + 60);
+
+        textAlign(LEFT, BASELINE);
+    }
+}
+
+function mousePressed() {
+    if (!music.isPlaying()) {
+        music.loop();
+    }
+}
+
+// lemon
+class Lemon {
+    constructor(x, y, s) {
+        this.x = x;
+        this.y = y;
+        this.s = s;
+        this.setNextMove();
+    }
+
+    setNextMove() {
+        this.nextMoveFrame = frameCount + floor(random(60, 180));
+    }
+
+    
+    respawn() {
+        this.x = random(60, width - 60);
+        this.y = random(100, height - 60);
+        this.setNextMove();
+    }
+
+    display() {
+        imageMode(CENTER);
+        image(lemonImg, this.x, this.y, this.s, this.s);
+    }
+}
+
+function mousePressed() {
+    if (!music.isPlaying()) {
+        music.loop();
+    }
+}
